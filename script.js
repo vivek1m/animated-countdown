0f0222139002dac21c707f0c59ee707034a857df
Vivek Sharma <156570752+vivek1m@users.noreply.github.com>
2025-06-26 21:54:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const nums = document.querySelectorAll('.nums span')
-const counter = document.querySelector('.counter')
-const finalMessage = document.querySelector('.final')
-const replay = document.querySelector('#replay')
-
-runAnimation()
-
-function resetDOM() {
-  counter.classList.remove('hide')
-  finalMessage.classList.remove('show')
-
-  nums.forEach((num) => {
-    num.classList.value = ''
-  })
-
-  nums[0].classList.add('in')
-}
 
